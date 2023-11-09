81d820e0bb318491e8c57c79807c57e448ac8018
henry-lewek <lewek@hartford.edu>
2023-11-08 20:06:49 -0500

    added code for user input

--- RunMatrix.java
+++ RunMatrix.java
@@ -1,6 +1,18 @@
 import java.util.Scanner;
 
 public class RunMatrix {
+
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+      System.out.print("Enter the size of the matrix: ");
+        int size = scanner.nextInt();
+
+      if (size <= 0) {
+        System.out.println("Please enter a positive number.");
+          return;
+
+      }
+       System.out.println("The size of your matrix is: " + size + " x " + size );
+    }
   }
-}
