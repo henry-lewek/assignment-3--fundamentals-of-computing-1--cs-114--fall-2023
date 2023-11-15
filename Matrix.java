822d0bedaf3166c4b5fb305ac3eeed6ee27eeb54
henry-lewek <lewek@hartford.edu>
2023-11-15 16:01:27 -0500

    completely fixed code

--- Matrix.java
+++ Matrix.java
@@ -1,8 +1,94 @@
+import java.util.Scanner;
+
 public class Matrix {
+  private int[][] matrix;
+  String PURPLE = "\u001B[35m";
+  String RESET = "\u001B[37m";
+  private void swap(int x1, int y1, int x2, int y2) {
+    int temp = matrix[x1][y1];
+    matrix[x1][y1] = matrix[x2][y2];
+    matrix[x2][y2] = temp;
+  }
 
-
-
-
-
-
+public Matrix(int size){
+  matrix = new int[size][size];
 }
+
+  // ----------------------
+  // Asks for + user input
+  // ----------------------
+  public static int inputMatrix() {
+    int size = 0;
+    try {
+        System.out.print("Enter the size of the matrix: ");
+        size = Integer.parseInt(System.console().readLine());
+
+        if (size <= 0) {
+            System.out.println("enter a positive number.");
+            return -1; // Return a int value to indicate an error
+        }
+
+        return size;
+    } catch (NumberFormatException arr) {
+        System.out.println("enter a positive number.");
+        return -1; // Return a int value to indicate an error
+    }
+}
+
+  // ----------------------------
+  // Reports matrix size to user
+  // ----------------------------
+  public static void reportMatrix(int size) {
+    System.out.println("The size of your matrix is: " + size + " x " + size);
+
+
+  }
+
+  // ------------------------
+  // Creates matrix with 0's
+  // ------------------------
+  public void printMatrix(int size) {
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j < size; j++) {
+        if(j == size - i - 1) {
+          System.out.printf("%s%d%s\t" , PURPLE , matrix[i][j] , RESET);
+        }
+        else {
+          System.out.printf("%d\t" , matrix[i][j]);
+        }
+
+      }
+      System.out.println();
+
+    }
+  }
+
+  // ----------------------------------
+  // Prints each element to the matrix
+  // ----------------------------------
+  public void populateMatrix(int size) {
+    int count = 1;
+    System.out.println("Printing matrix...");
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j < size; j++) {
+
+        matrix[i][j] = count;
+        count++;
+      }
+    }
+
+  }
+
+  // -------------------------------------
+  // Swaps the matrix except for diagonal
+  // -------------------------------------
+  public void flipMatrix(int size) {
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j < size; j++) {
+        if (i + j < size - 1) {
+          swap(i, j, size - 1 - i, size - 1 - j);
+        }
+      }
+    }
+  }
+    }
--- RunMatrix.java
+++ RunMatrix.java
@@ -1,90 +1,20 @@
 import java.util.Scanner;
 
 public class RunMatrix {
-  private static void swap(int[][] matrix, int x1, int y1, int x2, int y2) {
-    int temp = matrix[x1][y1];
-    matrix[x1][y1] = matrix[x2][y2];
-    matrix[x2][y2] = temp;
-    }
-
   public static void main(String[] args) {
 
-  //----------------------
-  // Asks for + user input
-  //----------------------
 
-    try (Scanner scanner = new Scanner(System.in)) {
-      System.out.print("Enter the size of the matrix: ");
-      int size = scanner.nextInt();
+        // Call inputMatrix to get the matrix size
+        int size = Matrix.inputMatrix();
 
-      if (size <= 0) {
-          System.out.println("Please enter a positive number.");
-          return;
-      }
+        Matrix myMatrix = new Matrix(size);
+        myMatrix.reportMatrix(size);
+        myMatrix.printMatrix(size);
+        myMatrix.populateMatrix(size);
+        myMatrix.printMatrix(size);
+        myMatrix.flipMatrix(size);
+        System.out.println("\nPrinting flipped matrix...\n");
 
-
- //----------------------------
- // Reports matrix size to user
- //----------------------------
-
-      System.out.println("The size of your matrix is: " + size + " x " + size);
-
-      int[][] matrix = new int[size][size];
-
- //------------------------
- // Creates matrix with 0's
- //------------------------
-
-      for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            System.out.print("0" + "\t");
-        }
-        System.out.println();
-      }
-
-      int count = 1;
-      for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-          matrix[i][j] = count;
-            count++;
-        }
-      }
-
- //----------------------------------
- // Prints each element to the matrix
- //----------------------------------
-
-      System.out.println("Printing matrix...");
-      for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-          System.out.print(matrix[i][j] + "\t");
-        }
-          System.out.println();
-      }
-
- //-------------------------------------
- // Swaps the matrix except for diagonal
- //-------------------------------------
-
-      for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-          if (i + j < size - 1) {
-            swap(matrix, i, j, size - 1 - j, size - 1 - i);
-          }
-        }
-      }
-
- //----------------------
- // Prints the new matrix
- //----------------------
-
-      System.out.println("\nPrinting flipped matrix...");
-      for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-          System.out.print(matrix[i][j] + "\t");
-        }
-          System.out.println();
-      }
+        myMatrix.printMatrix(size);
     }
-  }
 }
